7698c8be9062fe943efee53c13609b3908d7e305
peilerx <peiler.dany@gmail.com>
2026-01-02 17:29:00 +0200

    naming changes

--- benches/sum_bench.rs
+++ benches/sum_bench.rs
@@ -1,10 +1,10 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-pub fn native_sum(data: &[f32]) -> f32 {
+pub fn sum(data: &[f32]) -> f32 {
     data.iter().sum()
 }
 
-pub fn native_ilp_sum(data: &[f32]) -> f32 {
+pub fn prepped_sum(data: &[f32]) -> f32 {
     let mut acc0 = 0.0;
     let mut acc1 = 0.0;
     let mut acc2 = 0.0;
@@ -25,7 +25,7 @@ pub fn native_ilp_sum(data: &[f32]) -> f32 {
 }
 
 #[inline(never)]
-pub fn native_divide_f32(data: &[f32], divisor: f32) -> f32 {
+pub fn divide(data: &[f32], divisor: f32) -> f32 {
     let mut sum = 0.0;
     for &x in data {
         sum += x / divisor;
@@ -34,7 +34,7 @@ pub fn native_divide_f32(data: &[f32], divisor: f32) -> f32 {
 }
 
 #[inline(never)]
-pub fn prepped_divide_f32(data: &[f32], divisor: f32) -> f32 {
+pub fn prepped_divide(data: &[f32], divisor: f32) -> f32 {
     let mut acc0 = 0.0;
     let mut acc1 = 0.0;
     let mut acc2 = 0.0;
@@ -54,25 +54,23 @@ fn criterion_benchmark(c: &mut Criterion) {
     let size = 40_000_000;
     let data = vec![1.1f32; size];
 
-    let mut group_sum = c.benchmark_group("Summing_f32");
-    group_sum.bench_function("native", |b| b.iter(|| native_sum(black_box(&data))));
-    group_sum.bench_function("native-ilp-style", |b| {
-        b.iter(|| native_ilp_sum(black_box(&data)))
-    });
+    let mut group_sum = c.benchmark_group("Summing elements of [f32]");
+    group_sum.bench_function("native sum", |b| b.iter(|| sum(black_box(&data))));
+    group_sum.bench_function("prepped sum", |b| b.iter(|| prepped_sum(black_box(&data))));
     group_sum.finish();
 
     let size_div = 10_000_000;
     let data_div = vec![100.0f32; size_div];
     let divisor = black_box(7.0f32);
 
-    let mut group_div = c.benchmark_group("Division_f32");
+    let mut group_div = c.benchmark_group("Division elements of [f32]");
 
-    group_div.bench_function("native-divide-f32", |b| {
-        b.iter(|| native_divide_f32(black_box(&data_div), divisor))
+    group_div.bench_function("divide", |b| {
+        b.iter(|| divide(black_box(&data_div), divisor))
     });
 
-    group_div.bench_function("prepped-divide-f32-ilp", |b| {
-        b.iter(|| prepped_divide_f32(black_box(&data_div), divisor))
+    group_div.bench_function("prepped divide", |b| {
+        b.iter(|| prepped_divide(black_box(&data_div), divisor))
     });
 
     group_div.finish();
